270767793c65a667c37808b65c73254f0bbcfcca
Daniel Caldas <51807266+dsccdan@users.noreply.github.com>
2023-08-14 12:28:58 -0300

    upd whit news enemys

---
+++ Assets/GameFolder/Bat/Script/BatController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+
+    public Transform Player;
+
+    public float attackTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (GetComponent<Character>().life <= 0)
+        {
+            this.enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+
+
+        if (Vector2.Distance(transform.position, Player.position)>0.2f) {
+            attackTime = 0;
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, 0.5f * Time.deltaTime);
+        }
+        else
+        {
+            attackTime = attackTime + Time.deltaTime;
+            if (attackTime >= 1)
+            {
+                attackTime = 0;
+                Player.GetComponent<Character>().life--;
+            }
+        }
+        
+    }
+}
