f95915b42ac306983c8e86673100b6fac31149e8
Daniel Caldas <51807266+dsccdan@users.noreply.github.com>
2023-08-24 17:39:50 -0300

    upd plataforma

---
+++ Assets/GameFolder/Plataform/Script/PlataformController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformController : MonoBehaviour
+{
+    public Transform a;
+    public Transform b;
+    public float velPlataform;
+    public bool goRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goRight == true)
+        {
+
+            if (Vector2.Distance(transform.position, b.position) < 0.1f)
+            {
+                goRight = false;
+
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, b.position, velPlataform * Time.deltaTime);
+        }
+        else
+        {
+
+            if (Vector2.Distance(transform.position, a.position) < 0.1f)
+            {
+                goRight = true;
+
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, a.position, velPlataform * Time.deltaTime);
+        }
+    }
+    
+}
--- Assets/GameFolder/Player/Script/PlayerController.cs
+++ Assets/GameFolder/Player/Script/PlayerController.cs
@@ -125,4 +125,16 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Plataform"))
+            this.transform.parent = collision.transform;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Plataform"))
+            this.transform.parent = null;
+    }
+
 }
