8c3270db4db18cc00d29234b2d3432a65fc6b7b0
Daniel Caldas <51807266+dsccdan@users.noreply.github.com>
2023-09-30 11:51:51 -0300

    upd

--- Assets/GameFolder/powerUp/storm/stormWave.cs
+++ Assets/GameFolder/powerUp/storm/stormWave.cs
@@ -12,7 +12,7 @@ public class stormWave : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
