999025d2812f833e4bf8d37e444bb2ac7839f9b1
Daniel Caldas <dcaldas@ip.tv>
2024-03-31 12:38:37 -0300

    commit

--- Assets/Scripts/EnemyController/enemyController.cs
+++ Assets/Scripts/EnemyController/enemyController.cs
@@ -4,57 +4,28 @@ using UnityEngine;
 
 public class enemyController : MonoBehaviour
 {
-    int speed;
-    public Transform Skin;
+   
     public Transform enemyRanger;
-    GameObject Player;
+    
     public Transform lifeBar;
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.FindWithTag("Player");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed = Random.Range(3, 6);
+        
 
         if (GetComponent<CharacterEnemmy>().life <= 0)
         {
             enemyRanger.GetComponent<CircleCollider2D>().enabled = false;
             GetComponent<CapsuleCollider2D>().enabled = false;
-            this.enabled = false;
+            
             GetComponent<enemyController>().lifeBar.localScale = new Vector3(0, 1, 1);
         }
-
-        if (Skin.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-        {
-            return;
-        }
-
-
-        // Calculate the direction vector from the enemy to the player
-        Vector3 directionToPlayer = Player.transform.position - transform.position;
-        directionToPlayer.y = 0; // Make sure the enemy only follows on the X-axis
-
-        // Face the player's direction
-        if (directionToPlayer.x > 0)
-        {
-            Skin.localScale = new Vector3(1, 1, 1);
-        }
-        else if (directionToPlayer.x < 0)
-        {
-            Skin.localScale = new Vector3(-1, 1, 1);
-        }
-
-
-        // Move towards the player on the X-axis
-        float step = speed * Time.deltaTime;
-        Vector3 targetPosition = Player.transform.position;
-        targetPosition.y = transform.position.y; // Mantém a mesma altura do inimigo
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-
 
     }
     private void FixedUpdate()
---
+++ Assets/scripts/Enemywalk/enemyWalk.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyWalk : MonoBehaviour
+{
+    GameObject Player;
+    public Transform Skin;
+     int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Random.Range(3, 6);
+
+         if (Skin.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            return;
+        }
+
+
+        // Calculate the direction vector from the enemy to the player
+        Vector3 directionToPlayer = Player.transform.position - transform.position;
+        directionToPlayer.y = 0; // Make sure the enemy only follows on the X-axis
+
+        // Face the player's direction
+        if (directionToPlayer.x > 0)
+        {
+            Skin.localScale = new Vector3(1, 1, 1);
+        }
+        else if (directionToPlayer.x < 0)
+        {
+            Skin.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        // Move towards the player on the X-axis
+        float step = speed * Time.deltaTime;
+        Vector3 targetPosition = Player.transform.position;
+        targetPosition.y = transform.position.y; // Mant�m a mesma altura do inimigo
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+    }
+}
